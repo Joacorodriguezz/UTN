4496b186cbb60266b8f4d7b81e7a951182f0cd39
Joacorodriguezz <joaco.rodriguez800@gmail.com>
2023-05-13 00:00:00 -0300

    Corregido tp2

--- TP 2/banco.c
+++ TP 2/banco.c
@@ -1,27 +1,38 @@
+
 #include <stdio.h>
+#include <conio.h>
 
-float costo_total; 
-int precio_usd, cant_usd;
+int main() {
+    float precio_ars, costo_total, com;
+    int precio_usd, cant_usd;
 
-int main () {
-    printf("Ingrese el valor del Dólar: ");
+    printf("Ingrese el valor del Dolar: ");
     scanf("%d", &precio_usd);
-    printf("Ingrese la cantidad de dólares: ");
+    printf("Ingrese la cantidad de dolares: ");
     scanf("%d", &cant_usd);
     costo_total = precio_usd * cant_usd;
-    
+
     if (cant_usd < 501) {
-        printf("El costo de la transacción es: %f\n", costo_total);
-    } else if (cant_usd < 1501) {
-        printf("El costo de la transacción es: %f\n", costo_total + costo_total * 0.02);
-    } else if (cant_usd < 3501) {
-        printf("El costo de la transacción es: %f\n", costo_total + costo_total * 0.025);
+        com = 0;
+        precio_ars = costo_total;
+    } else if (cant_usd <= 1501) {
+        com = 0.02;
+        precio_ars = costo_total + costo_total * com;
+    } else if (cant_usd <= 3501) {
+        com = 0.025;
+        precio_ars = costo_total + costo_total * com;
     } else {
-        printf("El costo de la transacción es: %f\n", costo_total + costo_total * 0.04);
+        com = 0.041;
+        precio_ars = costo_total + costo_total * com;
     }
+
+    printf("//////////////////////////////////////\n");
+    printf("Se compraron: %d \n", cant_usd);
+    printf("El valor de cada dolar es: %d \n", precio_usd);
+    printf("La comision es: %f \n", com);
+    printf("El costo total de la transaccion en ars es: %f \n", precio_ars);
+    printf("//////////////////////////////////////");
+
     return 0;
 }
-
-
-    
 
